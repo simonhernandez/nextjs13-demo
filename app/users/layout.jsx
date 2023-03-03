680ee251549809b4ae893dbae12944cce2d7b8bd
Simon Barboza <simonbarboza@168.0.16.172.net-uno.net>
2023-03-03 15:39:51 -0400

    add: users page

---
+++ app/users/layout.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Users from './users'
+
+const UsersLayout = async ({ children }) => {
+  return (
+    <section className='flex'>
+        <aside className='w-1/4'>
+            <Users />
+        </aside>
+        <main>{ children }</main>
+    </section>
+  )
+}
+
+export default UsersLayout
---
+++ app/users/page.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Page = () => {
+  return (
+    <section>
+        <div className="p-4 sm:px-6 lg:px-8">
+            <h1 className='mb-3 text-2xl font-semibold tracking-wide'>Users</h1>
+            <p>Select a User</p>
+        </div>
+    </section>
+  )
+}
+
+export default Page
