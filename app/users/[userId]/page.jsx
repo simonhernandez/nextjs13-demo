991c1fbcd8c8f322794fd8841434cf3cc82b6d0a
Simon Barboza <simonbarboza@168.0.16.172.net-uno.net>
2023-03-03 15:48:39 -0400

    add: user page

---
+++ app/users/[userId]/page.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { getUserById } from '@/lib/prisma/users'
+import User from './user'
+
+
+const Page = async ({ params }) => {
+
+    const { user } = await getUserById(params.userId)
+
+  return (
+    <User user={user} />
+  )
+}
+
+export default Page
